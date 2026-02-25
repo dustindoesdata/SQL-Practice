1c8b44b6ce469b65a3dc0b945911242cf1e33f7b
dustindoesdata <dustindoesdata@gmail.com>
2026-02-25 00:24:10 -0800

    Completed 'Japanese Cities names' and updated README.

---
+++ HackerRank/Easy/JapaneseCitiesNames.sql
@@ -0,0 +1,17 @@
+/*
+Problem: Japanese Cities' Names
+Platform: HackerRank
+Difficulty: Easy
+Date Solved: 2026-02-25
+Environment: MS SQL Server
+
+Goal:
+"Query Names of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN."
+
+Notes:
+This one was strange since it's farther down in the exercises, but it's to pull all cities only.
+*/
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
