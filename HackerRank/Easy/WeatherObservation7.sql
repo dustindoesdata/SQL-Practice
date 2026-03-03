40f4e28625ff65f84814dfdd2045aaab2dc2d652
dustindoesdata <dustindoesdata@gmail.com>
2026-03-03 00:00:31 -0800

    Completed Weather  Observation 7

--- HackerRank/Easy/WeatherObservation7.sql
+++ HackerRank/Easy/WeatherObservation7.sql
@@ -1,11 +1,15 @@
 /*
-Problem:
-Platform:
-Difficulty:
-Date Solved: 
-Environment:
+Problem: Weather Observation Station 7
+Platform: HackerRank
+Difficulty: EASY
+Date Solved: 2026-03-02
+Environment: MS SQL SERVER
 
-Goal:
+Goal:Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
 
-Notes:
+Notes: Same as the last one, except moving the % (basically a wildcard operator) so wherever the letters are written is WHERE you care about.
 */
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '%[aeiou]';
