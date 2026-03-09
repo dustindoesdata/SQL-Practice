14142b09702e056fc926b8e69198e5a0cddba250
dustindoesdata <dustindoesdata@gmail.com>
2026-03-08 19:43:59 -0700

    Added 'New Companies' solution, and Updated README

--- HackerRank/Medium/NewCompanies.sql
+++ HackerRank/Medium/NewCompanies.sql
@@ -14,4 +14,36 @@ The company_code is string, so the sorting should not be numeric.
 For example, if the company_codes are C_1, C_2, and C_10, then the ascending company_codes will be C_1, C_10, and C_2.
 
 Notes:
+company_code
+founder name
+total # of lead managers
+total # of senior managers
+total # of managers
+total # of employees.
+ORDER BY company_code (string)
+handle DUPLICATES
 */
+
+SELECT DISTINCT
+    c.company_code
+    ,c.founder
+    ,COUNT(DISTINCT lm.lead_manager_code)
+    ,COUNT(DISTINCT sm.senior_manager_code)
+    ,COUNT(DISTINCT m.manager_code)
+    ,COUNT(DISTINCT e.employee_code)
+FROM
+    Company c
+    LEFT JOIN Lead_Manager lm
+        ON c.company_code = lm.company_code
+    LEFT JOIN Senior_Manager sm
+        ON lm.lead_manager_code = sm.lead_manager_code
+    LEFT JOIN Manager m
+        ON sm.senior_manager_code = m.senior_manager_code
+    LEFT JOIN Employee e
+        ON m.manager_code = e.manager_code
+GROUP BY
+    c.company_code,
+    c.founder
+ORDER BY
+    c.company_code ASC
+;
