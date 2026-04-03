5e9a8f1fdff48988868e7b01c4f7463e202a7685
dustindoesdata <dustindoesdata@gmail.com>
2026-04-02 20:46:16 -0700

    Added 'Top Competitor' problem details.

--- HackerRank/Medium/TopCompetitors.sql
+++ HackerRank/Medium/TopCompetitors.sql
@@ -16,4 +16,15 @@ received full scores in same number of challenges,
 then sort them by ascending hacker_id.
 
 Notes:
+Tables- Hackers, Difficulty, Challenges, Submissions
+
+Hackers table- hacker_id, name
+Difficulty table- difficulty_level, score
+Challenge table- challenge_id, hacker_id, difficulty_level
+Submissions table- submission_id, hacker_id, challenge_id, score
+
+Required Columns- hacker_id, name
+full scores > 1 challenge
+Order by total number of challenges with full score (desc)
+Order by hacker_id (asc)
 */
